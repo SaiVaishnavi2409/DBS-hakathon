eb5f782ae317b2543e95fed07de19b057b875b39
solanikhil <96112413+solanikhil@users.noreply.github.com>
2021-12-19 14:24:45 +0530

    Update admin2.sql

--- admin2.sql
+++ admin2.sql
@@ -1,5 +1,5 @@
 CREATE table Admin2 ( S.No. INt NOT NULL,Stock Name Varchar(255),Order Qty Int,Order Type Varchar(255),Executed Qty Int,Price Int,Order status Varchar(255),Order date FORMAT 'dd.mm.yyyy');
-Insert into Admin1( S.No.,Stock Name,Order Qty,Order Type ,Executed Qty ,Price,Order status,Order date) 
+Insert into Admin2( S.No.,Stock Name,Order Qty,Order Type ,Executed Qty ,Price,Order status,Order date) 
 VALUES(1,DBS,50,Limit,30,150,ACCEPTED,21.02.2021);
 Insert into Admin2( S.No.,Stock Name,Order Qty,Order Type ,Executed Qty,Order status,Order date) 
 VALUES(2,DBS,50,Market,30, ,ACCEPTED,21.02.2021);
