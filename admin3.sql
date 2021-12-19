2f82d8eb2a965310ee993fa1093908639f966eea
solanikhil <96112413+solanikhil@users.noreply.github.com>
2021-12-19 14:28:12 +0530

    Create admin3.sql

---
+++ admin3.sql
@@ -0,0 +1,7 @@
+CREATE table Admin3( S.No. INt NOT NULL,Stock Name Varchar(255),Order Qty Int,Order Type Varchar(255),Executed Qty Int,Price Int,Order status Varchar(255),Order date FORMAT 'dd.mm.yyyy');
+Insert into Admin3( S.No.,Stock Name,Order Qty,Order Type ,Executed Qty ,Price,Order status,Order date) 
+VALUES(1,DBS,50,Limit,50,150,EXECUTED,21.02.2021);
+Insert into Admin3( S.No.,Stock Name,Order Qty,Order Type ,Executed Qty,Order status,Order date) 
+VALUES(2,DBS,50,Market,50, ,EXECUTED,21.02.2021);
+Insert into Admin3( S.No.,Stock Name,Order Qty,Order Type ,Executed Qty,Price,Order status,Order date) 
+VALUES(3,DBS,50,Limit,0,140,REJECTED,21.02.2021);
